f37e91ebd2298f64cebcbede2603874a4e965758
miqbalramadhan18 <miqbalr18@gmail.com>
2023-10-27 13:59:56 +0700

    update

---
+++ gcfbackend_test.go
@@ -0,0 +1,69 @@
+package backend
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aiteung/atdb"
+	"github.com/whatsauth/watoken"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+// func TestUpdateGetData(t *testing.T) {
+// 	mconn := SetConnection("MONGOSTRING", "healhero_db")
+// 	datagedung := GetAllBangunanLineString(mconn, "healhero_db")
+// 	fmt.Println(datagedung)
+// }
+
+func TestGeneratePasswordHash(t *testing.T) {
+	password := "gischp5"
+	hash, _ := HashPassword(password) // ignore error for the sake of simplicity
+	fmt.Println("Password:", password)
+	fmt.Println("Hash:    ", hash)
+
+	match := CheckPasswordHash(password, hash)
+	fmt.Println("Match:   ", match)
+}
+func TestGeneratePrivateKeyPaseto(t *testing.T) {
+	privateKey, publicKey := watoken.GenerateKey()
+	fmt.Println(privateKey)
+	fmt.Println(publicKey)
+	hasil, err := watoken.Encode("Gisgcpcoba", privateKey)
+	fmt.Println(hasil, err)
+}
+
+func TestHashFunction(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "Gischp5coba")
+	var userdata User
+	userdata.Username = "Gischp5cobaweh"
+	userdata.Password = "GISGCPdotcom"
+
+	filter := bson.M{"username": userdata.Username}
+	res := atdb.GetOneDoc[User](mconn, "user", filter)
+	fmt.Println("Mongo User Result: ", res)
+	hash, _ := HashPassword(userdata.Password)
+	fmt.Println("Hash Password : ", hash)
+	match := CheckPasswordHash(userdata.Password, res.Password)
+	fmt.Println("Match:   ", match)
+
+}
+
+func TestIsPasswordValid(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "Gischp5coba")
+	var userdata User
+	userdata.Username = "Gischp5cobaweh"
+	userdata.Password = "GISGCPdotcom"
+
+	anu := IsPasswordValid(mconn, "user", userdata)
+	fmt.Println(anu)
+}
+
+func TestInsertUser(t *testing.T) {
+	mconn := SetConnection("MONGOSTRING", "Gischp5coba")
+	var userdata User
+	userdata.Username = "Gischp5cobaweh"
+	userdata.Password = "GISGCPdotcom"
+
+	nama := InsertUser(mconn, "user", userdata)
+	fmt.Println(nama)
+}
